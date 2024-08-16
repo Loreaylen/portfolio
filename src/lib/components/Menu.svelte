43c6705406dbfd1db094efef56d5dc0245361429
Loreaylen <lag9512@gmail.com>
2024-08-15 21:10:36 -0300

    Menu: Styles

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,29 @@
+<style>
+li {
+  list-style-type: none;
+}
+
+navbar {
+  width: 100%;
+}
+
+ul {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-around;
+}
+
+</style>
+
+<navbar>
+  <ul>
+    <li><a href=''>Sobre mí</a></li>
+    <li><a href='#'>Experiencia laboral</a></li>
+    <li><a href='#'>Proyectos</a></li>
+    <li><a href='#'>Tecnologías y herramientas</a></li>
+    <li><a href='#'>Contacto</a></li>
+  </ul>
+</navbar>
